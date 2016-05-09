3652b82c48d30b64fd4cfc6bffa76605bb04281a
jensvanloon <jens_vl@hotmail.com>
2016-05-10 00:04:53 +0200

    chart library updated

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-Chart.defaults.global.legend.display= false;
-
-window.onload = function () {
-var ctx = document.getElementById("web");
-var chart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["HTML5", "CSS3", "JavaScript", "SQL", "jQuery (ui)"],
-        datasets: [{
-            label: "Kennis op 10",
-            data: [8, 9, 7, 5, 6],
-            backgroundColor: ["#3793cf", "#e06d5c","#354458","#29aba4", "#ef89f7"]
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true,
-                    max: 10
-                }
-            }]
-        }
-    }
+new Chartist.Pie('.web', {
+    series: [20, 10, 30, 40]
+}, {
+    chartPadding: 30,
+    labelOffset: 50,
+    labelDirection: 'explode'
 });
-}
+
+var desktop = {
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+    series: [
+        [5, 2, 4, 2, 0]
+    ]
+};
+
+new Chartist.Line('.desktop', desktop);
+
+var server = {
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+    series: [
+        [5, 2, 4, 2, 0]
+    ]
+};
+
+new Chartist.Line('.server', server);
+
